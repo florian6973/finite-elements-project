48863497ab402f457f270cd9b4c0ca9e9c877367
florian6973 <flo1.raymond@gmail.com>
2021-11-23 23:01:23 +0100

    refactorisation code

---
+++ graph.py
@@ -0,0 +1,31 @@
+
+
+import matplotlib.pyplot as plt
+ 
+
+def main():
+    x = []
+    y = []
+    with open('plot.txt', 'r') as f:
+        lines = f.readlines()
+        for l in lines:
+            xt, yt = l.split(' ')
+            x.append(round(float(xt)*100, 2))
+            y.append(round(float(yt)*100, 2))
+    # plotting the points
+    plt.plot(x, y)
+    
+    # naming the x axis
+    plt.xlabel('Epaisseur e (cm)')
+    # naming the y axis
+    plt.ylabel('Déplacement vertical max d (cm)')
+    
+    # giving a title to my graph
+    plt.title("Evolution de l'affaissement en fonction de l'épaisseur du pont")
+    
+    # function to show the plot
+    plt.savefig('graph.png')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
